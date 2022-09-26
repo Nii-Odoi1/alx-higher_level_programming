0349a0e603cd732e5f37832b3973264b3c238d58
Nii Odoi <nii.odoi35@gmail.com>
2022-09-26 04:08:54 -0700

    second commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""script that fetches https://intranet.hbtn.io/status"""
+"""script that fetches https://alx-intranet.hbtn.io/status"""
 
 
 if __name__ == "__main__":
     import urllib.request
-    url = "https://intranet.hbtn.io/status"
+    url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as resp:
         data = resp.read()
         print("Body response:")
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@
 
 if __name__ == "__main__":
     import requests
-    response = requests.get("https://intranet.hbtn.io/status")
+    response = requests.get("https://alx-intranet.hbtn.io/status")
     content = response.text
     print("Body response:")
     print("\t- type: {}".format(type(content)))
